36ee96a634d7c131eab6f2210e315f8f04645222
SeveraTheDuck <severa.dev@gmail.com>
2026-06-20 22:17:44 +0300

    chore(coverage): code style

--- template/cmake/Coverage.cmake
+++ template/cmake/Coverage.cmake
@@ -7,15 +7,15 @@
 if(${PROJECT_NAME}_ENABLE_COVERAGE)
   if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     message(STATUS "[${PROJECT_NAME}] Coverage: source-based (Clang/llvm-cov)")
-    target_compile_options(project_options INTERFACE
-            -fprofile-instr-generate -fcoverage-mapping)
+    target_compile_options(project_options INTERFACE -fprofile-instr-generate -fcoverage-mapping)
     target_link_options(project_options INTERFACE -fprofile-instr-generate)
   elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     message(STATUS "[${PROJECT_NAME}] Coverage: arc-based (GCC/gcov)")
     target_compile_options(project_options INTERFACE --coverage)
     target_link_options(project_options INTERFACE --coverage)
   else()
-    message(FATAL_ERROR
-            "[${PROJECT_NAME}] Coverage requires Clang or GCC, got '${CMAKE_CXX_COMPILER_ID}'")
+    message(
+      FATAL_ERROR "[${PROJECT_NAME}] Coverage requires Clang or GCC, got '${CMAKE_CXX_COMPILER_ID}'"
+    )
   endif()
 endif()
